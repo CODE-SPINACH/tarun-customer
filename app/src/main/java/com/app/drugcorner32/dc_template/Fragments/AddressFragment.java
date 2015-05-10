2ed7bc4cd9f58e3b05c1e7d9d30d76fe19e58b0d
ravi gupta <ravigupta9363@gmail.com>
2015-05-10 12:16:46 +0530

    Semi Final Design is Going

--- app/src/main/java/com/app/drugcorner32/dc_template/Fragments/AddressFragment.java
+++ app/src/main/java/com/app/drugcorner32/dc_template/Fragments/AddressFragment.java
@@ -8,9 +8,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.app.drugcorner32.dc_template.Data.AddressDetails;
 import com.app.drugcorner32.dc_template.R;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class AddressFragment extends android.support.v4.app.Fragment {
 
@@ -46,11 +50,11 @@ public class AddressFragment extends android.support.v4.app.Fragment {
 
         final TextView firstNameView = (TextView) view.findViewById(R.id.addressTextView1);
         final TextView lastNameView = (TextView) view.findViewById(R.id.addressTextView2);
-        TextView phoneNoView = (TextView) view.findViewById(R.id.addressTextView3);
+        final TextView phoneNoView = (TextView) view.findViewById(R.id.addressTextView3);
         final TextView houseNameView = (TextView) view.findViewById(R.id.addressTextView4);
         final TextView streetNameView = (TextView) view.findViewById(R.id.addressTextView5);
-        TextView landamrkNameView = (TextView) view.findViewById(R.id.addressTextView6);
-        TextView pincodeNoView = (TextView) view.findViewById(R.id.addressTextView7);
+        final TextView landamrkNameView = (TextView) view.findViewById(R.id.addressTextView6);
+        final TextView pincodeNoView = (TextView) view.findViewById(R.id.addressTextView7);
 
         Typeface typeface=Typeface.createFromAsset(getActivity().getAssets(),"fonts/gothic.ttf");
 
@@ -73,7 +77,60 @@ public class AddressFragment extends android.support.v4.app.Fragment {
                 details.setStreetInfo(streetNameView.getText().toString());
                 details.setLandmark(streetNameView.getText().toString());
 
-                callback.startNotificationActivity(details);
+                Pattern patternFirstName = Pattern.compile("^[a-zA-Z\\s]+$",Pattern.CASE_INSENSITIVE);
+                Pattern patternLastName = Pattern.compile("^[a-zA-Z\\s]+$",Pattern.CASE_INSENSITIVE);
+                Pattern patternHouseName = Pattern.compile("\\w+",Pattern.CASE_INSENSITIVE);
+                Pattern patternStreetName = Pattern.compile("\\w+",Pattern.CASE_INSENSITIVE);
+                Pattern patternLandmark = Pattern.compile("^[a-zA-Z\\s]+$",Pattern.CASE_INSENSITIVE);
+                Pattern patternPhoneNumber = Pattern.compile("^[789]\\d{9}$");
+                Pattern patternPincode = Pattern.compile("^([0-9]{6})$");
+
+                Matcher matcherFirstName = patternFirstName.matcher(firstNameView.getText().toString());
+                Matcher matcherLastName = patternLastName.matcher(lastNameView.getText().toString());
+                Matcher matcherHouseName = patternHouseName.matcher(houseNameView.getText().toString());
+                Matcher matcherStreetName = patternStreetName.matcher(streetNameView.getText().toString());
+                Matcher matcherLandmark = patternLandmark.matcher(landamrkNameView.getText().toString());
+                Matcher matcherPhoneNumber = patternPhoneNumber.matcher(phoneNoView.getText().toString());
+                Matcher matcherPincode = patternPincode.matcher(pincodeNoView.getText().toString());
+
+                if(matcherFirstName.matches() && matcherLastName.matches() && matcherHouseName.matches() && matcherStreetName.matches() && matcherLandmark.matches() && matcherPhoneNumber.matches() && matcherPincode.matches())
+                {
+                    callback.startNotificationActivity(details);
+                }
+                else if(firstNameView.getText().toString().matches("") || lastNameView.getText().toString().matches("") || houseNameView.getText().toString().matches("") || streetNameView.getText().toString().matches("") || phoneNoView.getText().toString().matches("") || pincodeNoView.getText().toString().matches("") || landamrkNameView.getText().toString().matches(""))
+                {
+                    Toast.makeText(getActivity(), "One or more fields is empty", Toast.LENGTH_SHORT).show();
+                }
+                else if(!matcherFirstName.matches())
+                {
+                    Toast.makeText(getActivity(), "Enter Correct First Name", Toast.LENGTH_SHORT).show();
+                }
+                else if(!matcherLastName.matches())
+                {
+                    Toast.makeText(getActivity(), "Enter Correct Last Name", Toast.LENGTH_SHORT).show();
+                }
+                else if(!matcherPhoneNumber.matches())
+                {
+                    Toast.makeText(getActivity(), "Enter Correct Phone Number", Toast.LENGTH_SHORT).show();
+                }
+                else if(!matcherHouseName.matches())
+                {
+                    Toast.makeText(getActivity(), "Enter Correct Address", Toast.LENGTH_SHORT).show();
+                }
+                else if(!matcherStreetName.matches())
+                {
+                    Toast.makeText(getActivity(), "Enter Correct Address", Toast.LENGTH_SHORT).show();
+                }
+                else if(!matcherLandmark.matches())
+                {
+                    Toast.makeText(getActivity(), "Enter Correct Landmark", Toast.LENGTH_SHORT).show();
+                }
+                else if(!matcherPincode.matches())
+                {
+                    Toast.makeText(getActivity(), "Enter Correct Pincode", Toast.LENGTH_SHORT).show();
+                }
+
+
             }
         });
 
